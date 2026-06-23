ac33ee4fca72fbb4a9e18620b41016c6d8251284
Yasir Sel lam El Khadiri <yasirsellam8@gmail.com>
2026-06-23 19:22:02 +0200

    Redireccion y fix config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig: any = {
   eslint: {
     // Esto apaga el inspector de código estricto en Vercel
     ignoreDuringBuilds: true,
